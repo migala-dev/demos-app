0ef26827949e53816c00d435c62f3c7454ed3190
Erik Ivanov <erik.ivanov.n@gmail.com>
2021-08-12 04:13:21 -0500

    replacing authCubit

--- lib/core/enums/authentication_status.enum.dart
+++
@@ -1,1 +0,0 @@
-enum AuthenticationStatus { unknown, authenticated, unauthenticated }
--- lib/main.dart
+++ lib/main.dart
@@ -6,42 +6,39 @@ import 'package:fluro/fluro.dart';
 import 'package:demos_app/config/routes/application.dart';
 import 'package:demos_app/config/routes/routes.dart';
 import 'package:demos_app/config/themes/cubit/theme_cubit.dart';
-import 'package:demos_app/core/auth/cubit/auth_cubit.dart';
 
-import 'core/enums/authentication_status.enum.dart';
+import 'package:demos_app/utils/services/token.service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final bool userIsAuthenticate = await TokenService().isAuthenticate();
+
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
         create: (_) => ThemeCubit(),
-      ),
-      BlocProvider(create: (_) => AuthCubit())
+      )
     ],
-    child: DemosApp(),
+    child: DemosApp(
+        initialRoute: userIsAuthenticate ? Routes.spaces : Routes.login),
   ));
 }
 
 class DemosApp extends StatefulWidget {
+  final String initialRoute;
+
+  const DemosApp({Key? key, required this.initialRoute}) : super(key: key);
+
   @override
   State createState() => DemosAppState();
 }
 
 class DemosAppState extends State<DemosApp> {
-  final _navigatorKey = GlobalKey<NavigatorState>();
-  NavigatorState? get _navigator => _navigatorKey.currentState;
-
   DemosAppState() {
     final router = FluroRouter();
     Routes.configureRoutes(router);
     Application.router = router;
-  }
-
-  @override
-  void initState() {
-    final authCubit = context.read<AuthCubit>();
-    authCubit.checkUserIsAuthenticated();
-    super.initState();
   }
 
   @override
@@ -52,30 +49,11 @@ class DemosAppState extends State<DemosApp> {
     final app = BlocBuilder<ThemeCubit, ThemeData>(
       builder: (context, theme) {
         return MaterialApp(
-          navigatorKey: _navigatorKey,
           theme: theme,
           title: 'DemosApp',
           debugShowCheckedModeBanner: false,
-          initialRoute: Routes.authLoading,
           onGenerateRoute: Application.router.generator,
-          builder: (context, child) {
-            return BlocListener<AuthCubit, AuthState>(
-              listener: (context, state) {
-                switch (state.authenticationStatus) {
-                  case AuthenticationStatus.authenticated:
-                    _navigator?.pushNamedAndRemoveUntil(
-                        Routes.spaces, (route) => false);
-                    break;
-
-                  default:
-                    _navigator?.pushNamedAndRemoveUntil(
-                        Routes.login, (route) => false);
-                    break;
-                }
-              },
-              child: child,
-            );
-          },
+          initialRoute: widget.initialRoute,
         );
       },
     );
