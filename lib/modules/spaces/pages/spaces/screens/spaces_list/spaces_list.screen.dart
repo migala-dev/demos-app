3932f05361def577534defb08e0c8af05fe6ac15
Erik Ivanov <erik.ivanov.n@gmail.com>
2021-10-11 02:53:29 -0500

    dynamic spaces page

--- lib/modules/spaces/pages/spaces/screens/spaces_list/spaces_list.screen.dart
+++ lib/modules/spaces/pages/spaces/screens/spaces_list/spaces_list.screen.dart
@@ -2,6 +2,7 @@ import 'package:badges/badges.dart';
 import 'package:demos_app/config/routes/routes.dart';
 import 'package:demos_app/modules/spaces/models/space_view.model.dart';
 import 'package:demos_app/modules/spaces/pages/spaces/screens/spaces_list/widgets/spaces_scroll_view.widget.dart';
+import 'package:demos_app/modules/spaces/pages/spaces/utils/checkers.dart';
 import 'package:demos_app/modules/spaces/pages/spaces/widgets/popup_spaces_menu_button.widget.dart';
 import 'package:demos_app/widgets/wrappers/top_snakbars_notification.dart';
 import 'package:flutter/material.dart';
@@ -20,20 +21,9 @@ class SpaceListScreen extends StatelessWidget {
       length: 2,
       child: Scaffold(
           appBar: AppBar(
-            title: Text('Demos'),
+            title: getTitle(),
             actions: [PopupSpacesMenuButton()],
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Text('Espacios')),
-                Tab(
-                    icon: Badge(
-                        showBadge: invitations.length > 0,
-                        elevation: 0,
-                        position: BadgePosition(end: -28),
-                        badgeContent: Text('${invitations.length}'),
-                        child: Text('Invitaciones'))),
-              ],
-            ),
+            bottom: getSpacesTabBar(),
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
@@ -41,14 +31,47 @@ class SpaceListScreen extends StatelessWidget {
             },
             child: Icon(Icons.add),
           ),
-          body: TopSnakbarsNotification(
-            child: TabBarView(
-              children: [
-                SpacesScrollView(spaces: spaces),
-                SpacesScrollView(spaces: invitations),
-              ],
-            ),
-          )),
+          body: getBody()),
     );
+  }
+
+  Widget getBody() {
+    if (areOnlySpaceInvitations(spaces, invitations))
+      return SpacesScrollView(spaces: invitations);
+    if (areOnlySpaces(spaces, invitations))
+      return SpacesScrollView(spaces: spaces);
+
+    return TopSnakbarsNotification(
+      child: TabBarView(
+        children: [
+          SpacesScrollView(spaces: spaces),
+          SpacesScrollView(spaces: invitations),
+        ],
+      ),
+    );
+  }
+
+  Widget getTitle() {
+    if (areOnlySpaceInvitations(spaces, invitations))
+      return Text('Tus Invitaciones');
+    if (areOnlySpaces(spaces, invitations)) return Text('Tus Espacios');
+
+    return Text('Demos');
+  }
+
+  PreferredSizeWidget? getSpacesTabBar() {
+    if (requiresTabBar(spaces, invitations))
+      return TabBar(
+        tabs: [
+          Tab(icon: Text('Espacios')),
+          Tab(
+              icon: Badge(
+                  showBadge: invitations.length > 0,
+                  elevation: 0,
+                  position: BadgePosition(end: -28),
+                  badgeContent: Text('${invitations.length}'),
+                  child: Text('Invitaciones'))),
+        ],
+      );
   }
 }
