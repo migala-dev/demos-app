a12196f79c7933a727d4a50e0d38bfb7cf3d9a06
Alex Ortega <alexortegazj@gmail.com>
2021-10-17 09:54:06 -0700

    Custom space title

--- lib/modules/spaces/models/space_view.model.dart
+++ lib/modules/spaces/models/space_view.model.dart
@@ -3,13 +3,15 @@ class SpaceView {
   final String name;
   final String? pictureKey;
   final int membersCount;
+  final String? createdAt;
 
   SpaceView(
       {required this.spaceId,
       required this.name,
       this.pictureKey,
-      required this.membersCount});
+      required this.membersCount, 
+      this.createdAt});
 
   factory SpaceView.fromObject(Map<String, dynamic> o) =>
-      SpaceView(spaceId: o['id'], name: o['name'], membersCount: o['members']);
+      SpaceView(spaceId: o['id'], name: o['name'], membersCount: o['members'], createdAt: o['createdAt']);
 }
--- lib/modules/spaces/pages/spaces/screens/spaces_list/spaces_list.screen.dart
+++ lib/modules/spaces/pages/spaces/screens/spaces_list/spaces_list.screen.dart
@@ -1,9 +1,10 @@
 import 'package:badges/badges.dart';
 import 'package:demos_app/config/routes/routes.dart';
 import 'package:demos_app/modules/spaces/models/space_view.model.dart';
-import 'package:demos_app/modules/spaces/pages/spaces/screens/spaces_list/widgets/spaces_scroll_view.widget.dart';
+import 'package:demos_app/modules/spaces/pages/spaces/screens/spaces_list/widgets/space_list.widget.dart';
 import 'package:demos_app/modules/spaces/pages/spaces/utils/checkers.dart';
 import 'package:demos_app/modules/spaces/pages/spaces/widgets/popup_spaces_menu_button.widget.dart';
+import 'package:demos_app/shared/services/date.service.dart';
 import 'package:demos_app/widgets/wrappers/top_snakbars_notification.dart';
 import 'package:flutter/material.dart';
 
@@ -31,23 +32,45 @@ class SpaceListScreen extends StatelessWidget {
             },
             child: Icon(Icons.add),
           ),
-          body: getBody()),
+          body: getBody(context)),
     );
   }
 
-  Widget getBody() {
+  Widget getBody(BuildContext context) {
     if (areOnlySpaceInvitations(spaces, invitations))
-      return SpacesScrollView(spaces: invitations);
-    if (areOnlySpaces(spaces, invitations))
-      return SpacesScrollView(spaces: spaces);
+      return getInvitationList(context);
+    if (areOnlySpaces(spaces, invitations)) return getSpaceList(context);
 
     return TopSnakbarsNotification(
       child: TabBarView(
         children: [
-          SpacesScrollView(spaces: spaces),
-          SpacesScrollView(spaces: invitations),
+          getSpaceList(context),
+          getInvitationList(context),
         ],
       ),
+    );
+  }
+
+  Widget getSpaceList(BuildContext context) {
+    return SpaceListWidget(
+      spaces: spaces,
+      getSubtitle: (spaceView) => '${spaceView.membersCount} miembros',
+      onSpaceTab: (spaceView) {
+        Navigator.pushNamed(context, Routes.spacesDetails,
+            arguments: spaceView);
+      },
+    );
+  }
+
+  Widget getInvitationList(BuildContext context) {
+    return SpaceListWidget(
+      spaces: invitations,
+      getSubtitle: (spaceView) =>
+          'Invitado desde el ${DateService.parseToStandardDate(spaceView.createdAt!)}',
+      onSpaceTab: (spaceView) {
+        Navigator.pushNamed(context, Routes.spaceInvitation,
+            arguments: spaceView);
+      },
     );
   }
 
