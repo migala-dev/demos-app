02e497402f6bc6ac195ce42661ff20a8b78201c5
Alex Ortega <alexortegazj@gmail.com>
2021-11-10 08:52:44 -0700

    Moving the method to check if the user is admin to a service

--- lib/modules/spaces/validators/is_current_user_admin.widget_validator.dart
+++ lib/modules/spaces/validators/is_current_user_admin.widget_validator.dart
@@ -1,9 +1,7 @@
-import 'package:demos_app/core/enums/space-role.enum.dart';
-import 'package:demos_app/core/models/role_user_space.model.dart';
 import 'package:demos_app/core/models/space.model.dart';
 import 'package:demos_app/core/models/user.model.dart';
-import 'package:demos_app/core/repositories/role_user_space.repository.dart';
 import 'package:demos_app/core/services/current_user.service.dart';
+import 'package:demos_app/core/services/general_spaces.service.dart';
 import 'package:demos_app/modules/spaces/pages/spaces/services/current_space.service.dart';
 import 'package:demos_app/widgets/wrappers/safe_widget/widget_validator.interface.dart';
 
@@ -17,9 +15,7 @@ class IsCurrentUserAdminWidgetValidator implements WidgetValidator {
     }
 
     User? user = await CurrentUserService().getCurrentUser();
-    RoleUserSpace? userRole = await RoleUserSpaceRepository()
-        .findByUserIdAndSpaceId(user!.userId!, space.spaceId!);
 
-    return userRole != null ? userRole.role == SpaceRole.ADMIN : false;
+    return GeneralSpaceService().isUserAdmin(user!.userId!, space.spaceId!);
   }
 }
