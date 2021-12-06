fca0bfb43269a3167fc7a4490f2496c9d8e35993
Luis Meza <angel841bv@gmail.com>
2021-12-06 15:19:23 -0800

    Slider Error repair

--- lib/modules/spaces/pages/space_details/screens/space_percentage/space_percentage_settings.screen.dart
+++ lib/modules/spaces/pages/space_details/screens/space_percentage/space_percentage_settings.screen.dart
@@ -1,5 +1,6 @@
 import 'package:demos_app/modules/spaces/pages/new_space/screens/space_percentages_form/widgets/space_percentage.widget.dart';
 import 'package:demos_app/utils/ui/modals/open_confirmation_dialog.dart';
+import 'package:demos_app/widgets/wrappers/safe_widget/widget_validator.interface.dart';
 import 'package:flutter/material.dart';
 import 'package:demos_app/core/models/space.model.dart';
 import 'package:demos_app/modules/spaces/pages/space_details/services/space_details.service.dart';
@@ -22,6 +23,7 @@ class _SpacePercentageSettingsScreenState
     extends State<SpacePercentageSettingsScreen> with LoadingStateHandler {
   int approvalPercentage = 70;
   int participationPercentage = 70;
+  List<WidgetValidator>? validators = [IsCurrentUserAdminWidgetValidator()];
 
   @override
   void initState() {
@@ -42,16 +44,16 @@ class _SpacePercentageSettingsScreenState
         child: Column(
           children: [
             SpacePercentage(
-              initialValue: approvalPercentage,
-              onChange: (approvalPercentage) {
-                setState(() {
-                  this.approvalPercentage = approvalPercentage;
-                });
-              },
-              title: 'Porcentaje de aprovación',
-              subtitle: 'En votaciones A favor/En contra',
-              toBeApprovedLabel: ' para aprobar la propuesta.',
-            ),
+                initialValue: approvalPercentage,
+                onChange: (approvalPercentage) {
+                  setState(() {
+                    this.approvalPercentage = approvalPercentage;
+                  });
+                },
+                title: 'Porcentaje de aprovación',
+                subtitle: 'En votaciones A favor/En contra',
+                toBeApprovedLabel: ' para aprobar la propuesta.',
+                validators: validators),
             Container(
               height: 30.0,
               margin: const EdgeInsets.only(bottom: 12.0),
@@ -59,17 +61,17 @@ class _SpacePercentageSettingsScreenState
                   border: Border(bottom: BorderSide(color: greyColor))),
             ),
             SpacePercentage(
-              initialValue: participationPercentage,
-              onChange: (participationPercentage) {
-                setState(() {
-                  this.participationPercentage = participationPercentage;
-                });
-              },
-              title: 'Porcentaje de participación',
-              subtitle: 'En votaciones con opción multiple',
-              toBeApprovedLabel:
-                  ' para aprobar una de las opciones de una propuesta.',
-            ),
+                initialValue: participationPercentage,
+                onChange: (participationPercentage) {
+                  setState(() {
+                    this.participationPercentage = participationPercentage;
+                  });
+                },
+                title: 'Porcentaje de participación',
+                subtitle: 'En votaciones con opción multiple',
+                toBeApprovedLabel:
+                    ' para aprobar una de las opciones de una propuesta.',
+                validators: validators),
             const Spacer(),
             SafeWidgetValidator(
               validators: [IsCurrentUserAdminWidgetValidator()],
