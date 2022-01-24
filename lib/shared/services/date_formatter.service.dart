a931cf7fa6796b303bab59f7b1029ef08071b2cb
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-01-24 09:07:23 -0600

    parseToDayMonthYearDate

--- lib/shared/services/date_formatter.service.dart
+++ lib/shared/services/date_formatter.service.dart
@@ -14,4 +14,16 @@ class DateFormatterService {
       return date;
     }
   }
+
+  static String parseToDayMonthYearDate(String date) {
+    try {
+      DateFormat dateFormat = DateFormat('yyyy-MM-dd');
+      DateTime inputDate = dateFormat.parse(date);
+
+      var outputFormat = DateFormat('dd/MM/yyyy');
+      return outputFormat.format(inputDate);
+    } catch (e) {
+      return date;
+    }
+  }
 }
