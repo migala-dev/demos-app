2ec7b7cb8a0a66b7d82244899e4e15046e2b3f0a
Alex Ortega <alexortegazj@gmail.com>
2022-01-27 16:22:39 -0700

    Refactoring proposals view lists

--- lib/shared/constants/proposal_list_type_menu_order.dart
+++
@@ -1,8 +0,0 @@
-import 'package:demos_app/modules/proposals/proposals/enums/proposal_list_type.enum.dart';
-
-const List<ProposalListType> proposalListTypeMenuOrder = [
-  ProposalListType.draft,
-  ProposalListType.inProgress,
-  ProposalListType.recent,
-  ProposalListType.history
-];
