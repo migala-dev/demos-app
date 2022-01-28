a7b3c522d851f88ba70677ab105284390c544629
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-01-28 08:59:43 -0600

    proposalService path

--- lib/enviroments/enviroment_dev.dart
+++ lib/enviroments/enviroment_dev.dart
@@ -4,7 +4,7 @@ class EnviromentDev implements Enviroment {
   final String _authServiceUrl = 'http://44.194.95.212:3000/v1';
   final String _spaceServiceUrl = 'http://44.194.95.212:3001/v1';
   final String _cacheService = 'http://44.194.95.212:3002/v1';
-  final String _proposalsService = 'http://10.0.2.2:3003/v1';
+  final String _proposalsService = 'http://44.194.95.212:3003/v1';
   final String _websocketService = 'ws://44.194.95.212:5000';
 
   @override
