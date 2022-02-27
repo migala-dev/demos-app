7f831d87f60ebfee717c5895a4205997a88327b8
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-02-27 13:39:15 -0600

    EditProposalMenuOption

---
+++ lib/modules/proposals/pages/proposal_details/menu_options/edit_proposal.menu_option.dart
@@ -0,0 +1,30 @@
+import 'package:demos_app/config/routes/routes.dart';
+import 'package:demos_app/modules/proposals/pages/proposal_details/bloc/proposal_details.bloc.dart';
+import 'package:demos_app/modules/proposals/pages/proposal_form/bloc/proposal_form.bloc.dart';
+import 'package:demos_app/modules/proposals/pages/proposal_form/bloc/proposal_form_bloc.events.dart';
+import 'package:demos_app/modules/proposals/pages/proposal_form/models/proposal_form_view.model.dart';
+import 'package:demos_app/shared/interfaces/menu_option.interface.dart';
+import 'package:flutter/material.dart';
+
+class EditProposalMenuOption implements MenuOption {
+  @override
+  String name = 'Editar';
+  @override
+  IconData icon = Icons.edit;
+
+  @override
+  void onTap(BuildContext context) => goToEditProposal(context);
+
+  void goToEditProposal(BuildContext context) {
+    final proposal = ProposalDetailsBloc().state!;
+    final proposalFormView = ProposalFormView(
+        title: proposal.title ?? '',
+        content: proposal.content ?? '',
+        optionType: proposal.optionType,
+        isNew: false,
+        manifestoOptions: proposal.manifestoOptions);
+
+    ProposalFormBloc().add(ProposalFormSetProposalFormView(proposalFormView));
+    Navigator.pushNamed(context, Routes.proposalForm);
+  }
+}
--- lib/modules/proposals/pages/proposal_details/widgets/popup_proposal_details_menu_options.widget.dart
+++ lib/modules/proposals/pages/proposal_details/widgets/popup_proposal_details_menu_options.widget.dart
@@ -1,10 +1,14 @@
 import 'package:demos_app/modules/proposals/pages/proposal_details/menu_options/cancel_proposal.menu_option.dart';
+import 'package:demos_app/modules/proposals/pages/proposal_details/menu_options/edit_proposal.menu_option.dart';
 import 'package:demos_app/shared/interfaces/menu_option.interface.dart';
 import 'package:demos_app/widgets/general/popup_menu_options.widget.dart';
 import 'package:flutter/material.dart';
 
 class PopupProposalDetailsMenuOptions extends StatelessWidget {
-  final List<MenuOption> menuOptions = [CancelProposalMenuOption()];
+  final List<MenuOption> menuOptions = [
+    CancelProposalMenuOption(),
+    EditProposalMenuOption()
+  ];
 
   PopupProposalDetailsMenuOptions({Key? key}) : super(key: key);
 
