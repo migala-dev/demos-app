0bac1201702f0551d099adc5fca20dbd4fd8dfdd
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-03-10 14:07:12 -0600

    reaload proposal updated

--- lib/modules/proposals/pages/proposal_form/proposal_form_editors/in_progress.proposal_form_editor.dart
+++ lib/modules/proposals/pages/proposal_form/proposal_form_editors/in_progress.proposal_form_editor.dart
@@ -1,6 +1,9 @@
+import 'package:demos_app/modules/proposals/pages/proposal_details/bloc/proposal_details.bloc.dart';
+import 'package:demos_app/modules/proposals/pages/proposal_details/bloc/proposal_details_bloc.events.dart';
 import 'package:demos_app/modules/proposals/pages/proposal_form/bloc/proposal_form.bloc.dart';
 import 'package:demos_app/modules/proposals/pages/proposal_form/interfaces/proposal_form_editor.interface.dart';
 import 'package:demos_app/modules/proposals/pages/proposal_form/models/proposal_form_view.model.dart';
+import 'package:demos_app/modules/proposals/pages/proposals/services/proposal_view.service.dart';
 import 'package:demos_app/modules/proposals/services/proposal.service.dart';
 import 'package:demos_app/modules/spaces/pages/space_details/bloc/space.bloc.dart';
 import 'package:demos_app/utils/ui/modals/open_custom_confirmation.dialog.dart';
@@ -50,8 +53,14 @@ class InProgressProposalFormEditor implements ProposalFormEditor {
   void updateProposal() async {
     final spaceId = SpaceBloc().state.spaceId!;
     final ProposalFormView proposalFormView = ProposalFormBloc().state;
+    final proposalId = proposalFormView.proposalId!;
 
-    await ProposalService().updateProposal(
-        spaceId, proposalFormView.proposalId!, proposalFormView);
+    await ProposalService()
+        .updateProposal(spaceId, proposalId, proposalFormView);
+
+    final proposalUpdated =
+        await ProposalViewServie().getProposalViewByProposalId(proposalId);
+
+    ProposalDetailsBloc().add(SetProposalViewEvent(proposalUpdated!));
   }
 }
--- lib/modules/proposals/pages/proposals/repositories/proposal_view.repository.dart
+++ lib/modules/proposals/pages/proposals/repositories/proposal_view.repository.dart
@@ -31,45 +31,81 @@ class ProposalViewsRepository extends AppRepository {
   final colCreatedAt = ProposalRepository().colCreatedAt;
   final colManifestoOptionId = ManifestoOptionRepository().colId;
 
+  String _getSelectInnerJoinQuery() => '''
+    SELECT $tblManifesto.$colManifestoId,
+            $colProposalId,
+            $colTitle,
+            $colContent,
+            $colOptionType,
+            $tblProposals.$colCreatedBy,
+            $tblProposals.$colCreatedAt,
+            $tblProposals.$colSpaceId,
+            $colStatus,
+            $colProgressStatus,
+            $colUserName as "createdByName",
+            $colUserProfilePictureKey as "createdByProfilePictureKey",
+            (
+              select count(*) from $tblProposalParticipations
+              where $tblProposalParticipations.$colProposalId = $tblProposals.$colProposalId
+            ) as "votesCount"
+          FROM $tblManifesto
+          INNER
+            JOIN $tblProposals ON 
+                $tblManifesto.$colManifestoId = $tblProposals.$colManifestoId
+          INNER
+            JOIN $tblUsers ON
+                $tblUsers.$colUserId = $tblProposals.$colUpdatedBy
+  ''';
+
+  String _getFindByProposalIdQuery(String proposalId) => '''
+    ${_getSelectInnerJoinQuery()}
+    WHERE $colProposalId = '$proposalId'
+  ''';
+
   String _getFindBySpaceIdAndStatusQuery(
       String spaceId, ProposalStatus proposalStatus) {
     return """
-      SELECT $tblManifesto.$colManifestoId,
-        $colProposalId,
-        $colTitle,
-        $colContent,
-        $colOptionType,
-        $tblProposals.$colCreatedBy,
-        $tblProposals.$colCreatedAt,
-        $tblProposals.$colSpaceId,
-        $colStatus,
-        $colProgressStatus,
-        $colUserName as "createdByName",
-        $colUserProfilePictureKey as "createdByProfilePictureKey",
-        (
-          select count(*) from $tblProposalParticipations
-          where $tblProposalParticipations.$colProposalId = $tblProposals.$colProposalId
-        ) as "votesCount"
-      FROM $tblManifesto
-      INNER
-        JOIN $tblProposals ON 
-            $tblManifesto.$colManifestoId = $tblProposals.$colManifestoId
-      INNER
-        JOIN $tblUsers ON
-            $tblUsers.$colUserId = $tblProposals.$colUpdatedBy
+      ${_getSelectInnerJoinQuery()}
       WHERE $tblProposals.$colStatus = ${proposalStatus.index}
         AND $tblManifesto.$colSpaceId = '$spaceId'
     """;
   }
 
-  String _getManifestoOptionsQuery(String manifestoId) => 
-  """
+  String _getManifestoOptionsQuery(String manifestoId) => """
     SELECT 
       $colManifestoOptionId,
       $colTitle
     FROM $tblManifestoOptions
     WHERE $colManifestoId = '$manifestoId'
   """;
+
+  Future<List<Map<String, Object?>>> _getResultWithManifestoOptions(
+      List<Map<String, Object?>> result) async {
+    final Database? db = await this.db;
+
+    List<Map<String, Object?>> resultWithManifestoOptions =
+        result.map((r) => {...r}).toList();
+    for (Map<String, Object?> row in resultWithManifestoOptions) {
+      final String manifestoOptionsQuery =
+          _getManifestoOptionsQuery(row['manifestoId'].toString());
+      final manifestoOptionsResult = await db!.rawQuery(manifestoOptionsQuery);
+
+      row['manifestoOptions'] = manifestoOptionsResult;
+    }
+
+    return resultWithManifestoOptions;
+  }
+
+  Future<ProposalView?> findByProposalId(String proposalId) async {
+    Database? db = await this.db;
+    final result = await db!.rawQuery(_getFindByProposalIdQuery(proposalId));
+    List<Map<String, Object?>> resultWithManifestoOptions =
+        await _getResultWithManifestoOptions(result);
+
+    return resultWithManifestoOptions.isNotEmpty
+        ? ProposalView.fromObject(resultWithManifestoOptions[0])
+        : null;
+  }
 
   Future<List<ProposalView>> findAllBySpaceIdAndStatus(
       String spaceId, ProposalStatus proposalStatus) async {
@@ -79,15 +115,11 @@ class ProposalViewsRepository extends AppRepository {
     final result = await db!.rawQuery(query);
 
     List<Map<String, Object?>> resultWithManifestoOptions =
-        result.map((r) => {...r}).toList();
-    for (Map<String, Object?> row in resultWithManifestoOptions) {
-      final String manifestoOptionsQuery = _getManifestoOptionsQuery(row['manifestoId'].toString());
-      final manifestoOptionsResult = await db.rawQuery(manifestoOptionsQuery);
+        await _getResultWithManifestoOptions(result);
 
-      row['manifestoOptions'] = manifestoOptionsResult;
-    }
-
-    return resultWithManifestoOptions.map((row) => ProposalView.fromObject(row)).toList();
+    return resultWithManifestoOptions
+        .map((row) => ProposalView.fromObject(row))
+        .toList();
   }
 
   Future<int> getCountBySpaceIdAndStatus(
