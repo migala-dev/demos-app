8c6cc53d2809ed4cf16b5ba76c6743da7b12c923
Alex Ortega <alexortegazj@gmail.com>
2022-03-17 16:17:21 -0700

    Putting a license notice in each file. And adding the license file

--- lib/modules/spaces/screens/general_settings.screen.dart
+++ lib/modules/spaces/screens/general_settings.screen.dart
@@ -1,3 +1,22 @@
+/*
+  DEMOS
+  Copyright (C) 2022 Julian Alejandro Ortega Zepeda, Erik Ivanov Domínguez Rivera, Luis Ángel Meza Acosta
+  This file is part of DEMOS.
+
+  DEMOS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DEMOS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:demos_app/config/routes/routes.dart';
--- lib/widgets/profile/profile_field.widget.dart
+++ lib/widgets/profile/profile_field.widget.dart
@@ -1,3 +1,22 @@
+/*
+  DEMOS
+  Copyright (C) 2022 Julian Alejandro Ortega Zepeda, Erik Ivanov Domínguez Rivera, Luis Ángel Meza Acosta
+  This file is part of DEMOS.
+
+  DEMOS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DEMOS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:demos_app/widgets/wrappers/safe_widget/safe_widget_validator.dart';
 import 'package:demos_app/widgets/wrappers/safe_widget/widget_validator.interface.dart';
